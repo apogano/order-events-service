7a5bec1a9d7804495dde1b3b4a434becd79d9a1e
apogano <apogano@gmail.com>
2026-08-12 11:34:26 +0300

    Initialize Spring Boot project (Kafka, MongoDB dependencies)

---
+++ src/main/java/com/example/orderevents/OrderEventsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEventsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEventsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/orderevents/OrderEventsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEventsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
